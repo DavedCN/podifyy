1e0b539e88fec92e114986f877443dc4f6e7dd91
DavedCN <davednnadozie@gmail.com>
2024-05-07 20:10:54 +0100

    Fix Done

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,13 +11,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.VITE_API_KEY,
-  authDomain: import.meta.VITE_AUTH_DOMAIN,
-  projectId: import.meta.VITE_PROJECT_ID,
-  storageBucket: import.meta.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.VITE_APP_ID,
-  measurementId: import.meta.VITE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
